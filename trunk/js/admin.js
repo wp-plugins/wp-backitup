53c36c383cbdf31087b3c578be11aeda3de66715
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-13 00:52:26 +0000

    Tagging version 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-backitup@667156 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/js/admin.js
@@ -0,0 +1,51 @@
+/**
+ * WP Backitup Admin Control Panel JavaScripts
+ * 
+ * @version 1.1.3
+ * @since 1.1.3
+ */
+
+(function($){
+	//define backup variables
+	var backup = {
+		action: 'backup',
+		beforeSend: function() {
+			$('.backup-icon').css('visibility','visible');
+			$("#status").empty();
+			setInterval(display_log, 1000); 
+		}
+	};
+	//define download variables
+	var download = {
+		action: 'download'
+	};
+	//define logreader variables
+	var logreader = {
+		action: 'logreader'
+	};
+	//define logreader function
+	function display_log() {		
+		$.post(ajaxurl, logreader, function(response) {
+			$("#status").html(response);
+		});
+	}
+	//define download function
+	function download_link() {
+		$.post(ajaxurl, download, function(response) {
+			$("#download-link").html(response);
+		});
+	}
+
+	//execute download (on page load/refresh)
+	download_link();
+	
+	//execute backup on button click
+    $(".backup-button").click( function() {
+        $.post(ajaxurl, backup, function(response) {
+			download_link(); //Build download link
+			clearInterval(display_log); //Stop checking for status updates
+			$('.backup-icon').fadeOut(1000); //Fade process indicator
+        });   
+    })
+	
+})(jQuery);
