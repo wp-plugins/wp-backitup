fed747b4d42c49d7cd276d61730987452c236447
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-27 23:12:50 +0000

    Tagging version 1.1.6
    git-svn-id: https://plugins.svn.wordpress.org/wp-backitup/trunk@674343 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
  * @global    object    $wpdb
  * 
  * @author jcpeden
- * @version 1.1.5
+ * @version 1.1.6
  */
 /*
 Plugin Name: WP Backitup Lite
 Plugin URI: http://www.wpbackitup.com
 Description: Backup your content, settings, themes, plugins and media in just a few simple clicks.
-Version: 1.1.5
+Version: 1.1.6
 Author: John Peden
 Author URI: http://www.johncpeden.com
 License: GPL3
