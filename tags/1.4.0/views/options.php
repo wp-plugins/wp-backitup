d87088301aab1d0125f2e95a7cd5f7c052cf223b
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-06 13:10:37 +0000

    Tagging version 1.4.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-backitup@752286 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.4.0/views/options.php
@@ -0,0 +1,155 @@
+<script type="text/javascript">var __namespace = '<?php echo $namespace; ?>';</script>
+<div class="wrap">
+    <div id="wp-backitup-icon" class="icon32"><img src="<?php echo plugin_dir_url(dirname(__FILE__) ); ?>images/icon32.png" alt="WP Backitup Icon" height="32" width="32" /></div>
+    <h2><?php echo $page_title; ?></h2>
+    <div id="content">
+        <h3><?php _e('Backup', $namespace );?></h3>
+        <p><?php _e('Create a backup file of this site\'s content and settings', $namespace ) ;?></p>
+        <p><a href="#" class="backup-button button-primary"><?php _e( "Backup", $namespace ) ?></a><img class="backup-icon status-icon" src="<?php echo WPBACKITUP_URLPATH. "/images/loader.gif"; ?>" height="16" width="16" /></p>
+        <h3><?php _e('Download', $namespace );?></h3>
+        <p id="download-link"></p>
+        
+        <!--Disable restoration form if the user has not activated-->
+        <?php $status = $this->get_option( 'status' );
+                if( $status !== false && $status == 'valid' ) { ?>
+        <h3><?php _e('Restore', $namespace );?></h3>
+        <iframe id="upload_target" name="upload_target" src=""></iframe>
+        <p><?php _e('Restore a WP Backitup zip file and overwrite this site\'s content, themes, plugins, uploads and settings', $namespace );?></p>
+        <?php $max_upload = (int)(ini_get('upload_max_filesize'));
+            $max_post = (int)(ini_get('post_max_size'));
+            $memory_limit = (int)(ini_get('memory_limit'));
+            $upload_mb = min($max_upload, $max_post, $memory_limit); ?>
+        <p><?php _e( 'The maximum filesize you can upload is ', $namespace ); 
+            echo $upload_mb .'MB.'; ?>
+        </p>
+        <form id="restore-form" method="post" enctype="multipart/form-data" action="<?php echo WPBACKITUP_URLPATH .'/lib/includes/restore.php'; ?>"> 
+           <?php global $current_user; ?>
+            <input type="hidden" name="user_id" value="<?php echo $current_user->ID; ?>" />
+            <input type="hidden" name="maximum" id="maximum" value="<?php echo $upload_mb; ?>" />
+            <p><input name="wpbackitup-zip" id="wpbackitup-zip" type="file" /></p> 
+            <p><input type="submit" class="restore-button button-primary" name="restore" value="<?php _e( "Restore", $namespace ) ?>" /><img class="restore-icon status-icon" src="<?php echo WPBACKITUP_URLPATH. "/images/loader.gif"; ?>" height="16" width="16" /></p>
+        </form>
+        <?php } ?>
+        <!--End of restoration form-->
+        
+        <h3><?php _e('Status', $namespace );?></h3>
+        <div id="status">
+
+            <!--default status message-->
+            <ul class="default-status">
+                <li><?php _e('Nothing to report', $namespace );?></li>
+            </ul>
+
+            <!--backup status messages-->
+            <ul class="backup-status">
+                <li class='prerequisites'><?php _e('Preparing to backup', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='backupfiles'><?php _e('Backing-up /wp-content/', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='backupdb'><?php _e('Backing-up database', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='infofile'><?php _e('Creating backup directory', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='zipfile'><?php _e('Zipping backup directory', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='cleanup'><?php _e('Cleaning up', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='finalinfo'><span class='status'><?php _e('Backup file created successfully. You can download your backup file using the link above', $namespace ); ?></span></li>
+            </ul>
+
+            <!--backup error messages-->
+            <ul class="backup-errors">
+                <li class="error101"><span class='status error'><?php _e('Error: Unable to create new directory for backup. Please check your CHMOD settings of your wp-backitup plugin directory' , $namespace ); ?>.</span></li>
+                <li class="error102"><span class='status error'><?php _e('Error: Cannot create backup directory. Please check the CHMOD settings of your wp-backitup plugin directory', $namespace ); ?>.</span></li>
+                <li class="error103"><span class='status error'><?php _e('Error: Unable to backup your files. Please try again', $namespace ); ?>.</span></li>
+                <li class="error104"><span class='status error'><?php _e('Error: Unable to backup your database. Please try again', $namespace ); ?>.</span></li>
+                <li class="error105"><span class='status error'><?php _e('Error: Unable to create site information file. Please try again', $namespace ); ?>.</span></li>
+                <li class="error106"><span class='status error'><?php _e('Warning: Unable to cleanup your backup directory', $namespace ); ?>.</span></li>
+            </ul>
+
+            <!--restore status messages-->
+            <ul class="restore-status">
+                <li class="upload"><?php _e('Uploading restoration zip', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class="unzipping"><?php _e('Unzipping', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class="validation"><?php _e('Validating restoration zip', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class="restore_point"><?php _e('Setting checkpoint', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class="database"><?php _e('Importing database', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class="wpcontent"><?php _e('Importing /wp-content/ directory', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class="cleanup"><?php _e('Cleaning up', $namespace );?>...<span class='status'><?php _e('Done', $namespace );?></span><span class='fail error'><?php _e('Failed', $namespace );?></span></li>
+                <li class='finalinfo'><span class='status'><?php _e('Restoration completed successfully. Please refresh the page and login to the site again (with your current username and password)', $namespace ); ?></span></li>
+            </ul>
+
+            <!--restore error messages-->
+            <ul class="restore-errors">
+                <li class="error201"><span class='status error'><?php _e('Error: No file selected', $namespace ); ?>.</span></li>
+                <li class="error202"><span class='status error'><?php _e('Error: Your file could not be uploaded', $namespace ); ?>.</span></li>
+                <li class="error203"><span class='status error'><?php _e('Error: Your restoration file could not be unzipped', $namespace ); ?>.</span></li>
+                <li class="error204"><span class='status error'><?php _e('Error: Your zip file appears to be invalid. Please ensure you chose the correct zip file', $namespace ); ?>.</span></li>
+                <li class="error205"><span class='status error'><?php _e('Error: Cannot create restore point', $namespace ); ?>.</span></li>
+                <li class="error206"><span class='status error'><?php _e('Error: Unable to connect to your database', $namespace ); ?>.</span></li>
+                <li class="error207"><span class='status error'><?php _e('Error: Unable to get current site URL from database. Please try again', $namespace ); ?>.</span></li>
+                <li class="error208"><span class='status error'><?php _e('Error: Unable to get current home URL from database. Please try again', $namespace ); ?>.</span></li>
+                <li class="error209"><span class='status error'><?php _e('Error: Unable to get current user ID from database. Please try again', $namespace ); ?>.</span></li>
+                <li class="error210"><span class='status error'><?php _e('Error: Unable to get current user password from database. Please try again', $namespace ); ?>.</span></li>
+                <li class="error211"><span class='status error'><?php _e('Error: Unable to get current user email from database. Please try again', $namespace ); ?>.</span></li>
+                <li class="error212"><span class='status error'><?php _e('Error: Unable to get import your database. This may require importing the file manually', $namespace ); ?>.</span></li>
+                <li class="error213"><span class='status error'><?php _e('Error: Unable to update your current site URL value. This may require importing the file manually', $namespace ); ?>.</span></li>
+                <li class="error214"><span class='status error'><?php _e('Error: Unable to update your current home URL value. This may require importing the file manually', $namespace ); ?>.</span></li>
+                <li class="error215"><span class='status error'><?php _e('Error: Unable to update your user information. This may require importing the file manually', $namespace ); ?>.</span></li>
+                <li class="error216"><span class='status error'><?php _e('Error: Warning: Database not detected in import file', $namespace ); ?>.</span></li>
+                <li class="error217"><span class='status error'><?php _e('Error: Unable to remove existing wp-content directory for import. Please check your CHMOD settings in /wp-content/', $namespace ); ?>.</span></li>
+                <li class="error218"><span class='status error'><?php _e('Error: Unable to create new wp-content directory for import. Please check your CHMOD settings in /wp-content/', $namespace ); ?>.</span></li>
+                <li class="error219"><span class='status error'><?php _e('Error: Unable to import wp-content. Please try again', $namespace ); ?>.</span></li>
+                <li class="error220"><span class='status error'><?php _e('Warning: Unable to cleanup import directory', $namespace ); ?>.</span></li>
+            </ul>
+        </div>   
+        <?php if (site_url() == 'http://localhost/wpbackitup') {
+            echo '<p><div id="php">PHP messages here</p></div>'; 
+        } ?>
+    </div>
+    <div id="sidebar">
+        <form action="" method="post" id="<?php echo $namespace; ?>-form">
+        <?php wp_nonce_field( $namespace . "-update-options" ); ?>
+        <div class="widget">
+            <h3 class="promo"><?php _e('License Key', $namespace ); ?></h3>
+            <?php $license = $this->get_option( 'license_key' );
+                $status = $this->get_option( 'status' );
+                if( $status !== false && $status == 'valid' ) { ?>
+                    <p><?php _e('Pro features and auto-updates enabled', $namespace ); ?></p>
+                <?php } else { ?>
+                    <p><?php _e('Activate auto-restore and auto-updates by entering your license key', $namespace ); ?></p>
+                <?php } ?>
+                <p><input type="text" name="data[license_key]" id="license_key" value="<?php echo $license; ?>" />
+                <?php if( false !== $license ) { 
+                    if( $status !== false && $status == 'valid' ) { ?>
+                        <span style="color:green;"><?php _e('Active', $namespace); ?></span></p>
+                        <p class="submit"><input type="submit" name="Submit" class="button-secondary" value="<?php _e( "Update", $namespace ) ?>" /></p>
+                    <?php } else { ?>
+                        <span style="color:red;"><?php _e('Inactive', $namespace); ?></span></p>
+                        <p class="submit"><input type="submit" name="Submit" class="button-secondary" value="<?php _e( "Activate", $namespace ) ?>" /></p>
+                        <p><a href="http://www.wpbackitup.com/wp-backitup-pro"><?php _e('Purchase a license key', $namespace); ?></a></p>
+                    <?php } 
+                } ?>
+        </div>             
+        
+        <div class="widget">
+            <?php if( $status !== false && $status == 'valid' ) {
+                    $support_anchor = __('WP Backitup support system',$namespace);
+                    $support_url = 'http://www.wpbackitup.com/support/';
+                } else {
+                    $support_anchor = __('support system',$namespace);
+                    $support_url = 'http://wordpress.org/support/plugin/wp-backitup';
+                } ?>
+            <h3 class="promo"><?php _e('Need Help?', $namespace ); ?></h3>
+            <p><?php _e('Access the',$namespace); ?> <a href="<?php echo $support_url; ?>"><?php echo $support_anchor; ?></a>.</p>
+        </div>        
+        
+        <div class="widget">
+            <h3 class="promo"><?php _e('Spread the Word', $namespace ); ?></h3>
+            <p><a href="http://wordpress.org/extend/plugins/wp-backitup/"><?php _e('Rate WP Backitup', $namespace ); ?> 5&#9733;</a></p>
+        </div>
+
+        <div class="widget">
+            <h3 class="promo">Presstrends</h3>
+                <p><input type="radio" name="data[presstrends]" value="enabled" <?php if($this->get_option( 'presstrends' ) == 'enabled') echo 'checked'; ?>> <label><?php _e('Enable', $namespace ); ?></label></p>
+                <p><input type="radio" name="data[presstrends]" value="disabled" <?php if($this->get_option( 'presstrends' ) == 'disabled') echo 'checked'; ?>> <label><?php _e('Disable', $namespace ); ?></label></p>
+                <p><?php _e('Help to improve Easy Webtrends by enabling', $namespace ); ?> <a href="http://www.presstrends.io" target="_blank">Presstrends</a>.</p>
+                <p class="submit"><input type="submit" name="Submit" class="button-primary" value="<?php _e( "Save", $namespace ) ?>" /></p>
+        </div>
+        </form>
+    </div>
+</div>
