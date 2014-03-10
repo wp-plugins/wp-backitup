2ef2f72ad495d0c9b662d04263fa905766615494
cssimmon <cssimmon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-10 15:17:13 +0000

    Update to version 1.6.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-backitup@872788 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -9,7 +9,7 @@
  * @since 1.0.1
  */
 
-if( !defined( 'WPBACKITUP_VERSION' ) ) define( 'WPBACKITUP_VERSION', '1.6.2' );
+if( !defined( 'WPBACKITUP_VERSION' ) ) define( 'WPBACKITUP_VERSION', '1.6.3' );
 
 if( !defined( 'WPBACKITUP_DIRNAME' ) ) define( 'WPBACKITUP_DIRNAME', dirname( dirname( __FILE__ ) ) );
 
